793d0d4b06df0870d7c315731ad61aabd9a58de6
NiranjanaAJit <niranjana3121@gmail.com>
2024-07-07 16:58:27 +0530

    fix: entity not found issue

--- src/middleware/authorize.middleware.ts
+++ src/middleware/authorize.middleware.ts
@@ -4,11 +4,12 @@ import { jwtPayload, RequestWithUser } from "../utils/jwtPayload.types";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT_SECRET } from "../utils/constants";
 
+
 const authorize = async (request: RequestWithUser, response: Response , next:NextFunction) => {
     try{
 
 
-        const token = request.header("Authorization");
+        const token = getTokenFromRequestHeader(request);
         const payload  = jsonwebtoken.verify(token,JWT_SECRET);
         request.name = (payload as jwtPayload).name;
         request.email = (payload as jwtPayload).email;
@@ -20,5 +21,13 @@ const authorize = async (request: RequestWithUser, response: Response , next:Nex
     }
 
 }
+
+const getTokenFromRequestHeader = (req: RequestWithUser) => {
+    const bearerToken = req.header("Authorization");    
+    console.log("bearerToken: ", bearerToken);
+    const token = bearerToken ? bearerToken.replace("Bearer ", "") : "";
+    return token;
+}
+
 
 export default authorize;
